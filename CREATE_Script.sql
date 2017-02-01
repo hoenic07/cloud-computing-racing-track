23e3f05be5cfa9b7b1dfe691626de18a971e2f27
Magdalena Radinger <magda@magdalenasmbp3.home>
2017-02-01 16:32:57 +0100

    ADD sinatra-server

---
+++ CREATE_Script.sql
@@ -0,0 +1,43 @@
+-- Table: public.Position
+
+-- DROP TABLE public.Position;
+
+CREATE TABLE public.Position
+(
+    RacingTrackId integer NOT NULL,
+    Timestamp bigint NOT NULL,
+    Latitude double precision NOT NULL,
+    Longitude double precision NOT NULL,
+    CONSTRAINT Position_pkey PRIMARY KEY (RacingTrackId, Timestamp),
+    CONSTRAINT RacingTrackId FOREIGN KEY (RacingTrackId)
+        REFERENCES public.RacingTrack (Id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.Position
+    OWNER to postgres;
+
+
+-- Table: public.RacingTrack
+
+-- DROP TABLE public.RacingTrack;
+
+CREATE TABLE public.RacingTrack
+(
+    Id integer NOT NULL,
+    Name character varying COLLATE pg_catalog.default NOT NULL,
+    Finalized boolean NOT NULL,
+    CONSTRAINT RacingTrack_pkey PRIMARY KEY (Id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.RacingTrack
+    OWNER to postgres;
